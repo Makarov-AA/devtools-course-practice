819383863653e22f43fda7bde15c580fecec7eba
Makarov-AA <43129398+Makarov-AA@users.noreply.github.com>
2021-04-01 19:24:07 +0300

    Tabs to spaces

--- modules/complex-number/test/test_makarov_alexander_complex_number.cpp
+++ modules/complex-number/test/test_makarov_alexander_complex_number.cpp
@@ -18,7 +18,7 @@ TEST(Makarov_Alexander_ComlexNumberTest, Is_i_Square_Minus_One) {
     double real = 0.0;
     double img = 1.0;
     ComplexNumber z1(real, img);
-	ComplexNumber result = z1 * z1;
+    ComplexNumber result = z1 * z1;
     ASSERT_DOUBLE_EQ(-1.0, result.getRe());
     ASSERT_DOUBLE_EQ(0.0, result.getIm());
 }
@@ -29,9 +29,9 @@ TEST(Makarov_Alexander_ComlexNumberTest, Conjugates_Multiplication) {
     double real_2 = 5.0;
     double img_2 = 3.0;
     ComplexNumber z1(real_1, img_1), z2(real_2, img_2);
-	double expected_re = 34.0;
+    double expected_re = 34.0;
     double expected_im = 0.0;
-	ComplexNumber result = z1 * z2;
+    ComplexNumber result = z1 * z2;
     ASSERT_DOUBLE_EQ(expected_re, result.getRe());
     ASSERT_DOUBLE_EQ(expected_im, result.getIm());
 }
